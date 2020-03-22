8534237f26357cacf1ab9d85a3b6e47ebc7e83ad
Mario Morra <mail@mariomorra.com>
2020-03-21 21:54:48 -0400

    fixed author commit count, better console output

--- mgl843_final_script.py
+++ mgl843_final_script.py
@@ -16,11 +16,13 @@ repo_file = 'repos.txt'
 print(str.format("Reading Git repo text file '{repoFile}'...", repoFile=repo_file))
 with open(repo_file) as f: 
     lines = [line.rstrip('\n') for line in f] 
-
-repositoryCounter = 1
+repositoryCounter = 0
+("----------------------------------------------------------------------------------")
 
 # Loop on all repos in file
 for gitURL in lines:
+    
+    repositoryCounter = repositoryCounter + 1
 
     # Get project name from URL
     p = Path(gitURL)
@@ -28,30 +30,34 @@ for gitURL in lines:
 
     # Setup directory names
     newRepo = repo_dir + '/' + projectName
-    downloadRepo = newRepo + '/' + projectName + '_repo'
-    repositoryCounter = repositoryCounter + 1
+    downloadRepo = newRepo + '/' + projectName + '_repo'  
 
     # Clone repo into directory
     print(str.format("Cloning repo '{gitURL}' into '{downloadRepo}'...", gitURL=gitURL, downloadRepo=downloadRepo))
     Repo.clone_from(gitURL, downloadRepo)
     print("Repo cloned!")
+    print("----------------------------------------------------------------------------------")
 
+    # Create CSV file for commits
     commitsCSVFilePath = newRepo + '/' + projectName + '_commits.csv'
+    commitsFile = io.open(commitsCSVFilePath,"w+", encoding="utf-8")
     print(str.format("'{commitsCSVFilePath}' file created.", commitsCSVFilePath=commitsCSVFilePath))
     
-    # Create CSV file for commits
-    commitsFile = io.open(commitsCSVFilePath,"w+", encoding="utf-8")
-    print(str.format("Writing to '{commitsCSVFilePath}'...", commitsCSVFilePath=commitsCSVFilePath))
-    
     # Write info into commit file
+    print(str.format("Writing to '{commitsCSVFilePath}'...", commitsCSVFilePath=commitsCSVFilePath))
     commitsFile.write('"commit.hash","commit.author.name","commit.author.email","commit.author_date","mod.filename","mod.change_type","mod.nloc"\n') 
+    commitsCounter = 0
     for commit in RepositoryMining(downloadRepo).traverse_commits():
-        for mod in commit.modifications:       
+        commitsCounter = commitsCounter + 1
+        print(str.format("Analyzing commit #{currentCommit} ({hash})...", currentCommit=commitsCounter, hash=commit.hash))
+        for mod in commit.modifications:    
             commitsFile.write('"' + commit.hash + '","' + commit.author.name + '","' + commit.author.email + '","' + str(commit.author_date) + '","' + mod.filename + '","' + str(mod.change_type) + '","' + str(mod.nloc) + '"\n') 
-    
+
     # Close commit file
     commitsFile.close()
     print(str.format("'{commitsCSVFilePath}' completed and closed.", commitsCSVFilePath=commitsCSVFilePath))
+    print(str.format("Analyzed {nbCommits} commits.", nbCommits=commitsCounter))
+    print("----------------------------------------------------------------------------------")
 
     # Run PMD and save results into CSV file
     smellsCSVFilePath = newRepo + '/' + projectName + '_pmd_smells.csv'
@@ -59,11 +65,14 @@ for gitURL in lines:
     pmdCacheFile = './pmdCacheFile'
     subprocess.run(str.format("pmd.bat -d {repo} -R rulesets/java/design.xml -f csv -reportfile {file} -cache {cache} -shortnames", repo=downloadRepo, file=smellsCSVFilePath, cache=pmdCacheFile))
     print(str.format("PMD complete. Data saved to '{smellsCSVFilePath}'.", smellsCSVFilePath=smellsCSVFilePath))
+    print("----------------------------------------------------------------------------------")
 
-    # Get number of commits per author (doesn't work right now)
-    # nbCommitsPerAuthorFile = newRepo + '/' + projectName + '_nb_commits_per_author.csv'
-    # print("Getting number of commits per author...")
-    # subprocess.run(str.format("git shortlog -s -n --all --no-merges > {nbCommitsFile}.csv", nbCommitsFile=nbCommitsPerAuthorFile))
-    # print(str.format("Data saved to '{nbCommitsFile}'.", nbCommitsFile=nbCommitsPerAuthorFile))
+    # Get number of commits per author (careful: CSV seperated by TABS)
+    nbCommitsPerAuthorFile = newRepo + '/' + projectName + '_nb_commits_per_author.csv'
+    print("Getting number of commits per author...")
+    subprocess.run(str.format("git -C {dir} shortlog -s -n --all --no-merges > {outfile}", dir=downloadRepo, outfile=nbCommitsPerAuthorFile), shell=True)
+    print(str.format("Data saved to '{nbCommitsFile}'.", nbCommitsFile=nbCommitsPerAuthorFile))
+    print("----------------------------------------------------------------------------------")
 
 print(str.format("All done! Check in '{repoDir}' directory.", repoDir=repo_dir))
+print(str.format("Analyzed {nbRepos} repositories.", nbRepos=repositoryCounter))
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from pydriller import RepositoryMining
+from pydriller.metrics.process.contributors_experience import ContributorsExperience
+from pydriller.metrics.process.contributors_count import ContributorsCount
 from git import Repo
 import shutil
 import os
@@ -8,23 +10,21 @@ import io
 from pathlib import Path
 
 
-# pmd.bat -d C:\REPOS\MGL843\Python_RepoDownloder\DUMP\repo1 -R rulesets/java/design.xml -f csv -reportfile C:\REPOS\MGL843\Python_RepoDownloder\DUMP\repo1_smells.csv -cache C:\REPOS\MGL843\Python_RepoDownloder\pmdCacheFile
 
-# for commit in RepositoryMining(newRepo).traverse_commits():    
-#     for mod in commit.modifications:       
-#         f.write(commit.hash + ';' 
-#             + commit.author.name + ';' 
-#             + str(commit.author_date) + ';' 
-#             + mod.filename + ';' 
-#             + str(mod.nloc) + ';' 
-#             + str(mod.complexity)
-#             + '\n')
+metric = ContributorsCount(path_to_repo='E:/REPOS/MGL843/Python_RepoMiner/DUMP/zookeeper/zookeeper_repo', from_commit='047d9258a4730791b85cc81b0e1435465a32acbf' to_commit='8148f966947d3ecf3db0b756d93c9ffa88174af9')
+
+count = metric.count()
+minor = metric.count_minor()
+print('Number of contributors per file: {}'.format(count))
+print('Number of "minor" contributors per file: {}'.format(minor))
+
+
+
+
+
+
 
 # https://github.com/apache/spark
 # https://github.com/apache/kafka
 # https://github.com/apache/cassandra
-# https://github.com/apache/zookeeper
 # https://github.com/apache/hive
-
-
-# git shortlog -s -n --all --no-merges > spark_nb_commits_per_author.csv
